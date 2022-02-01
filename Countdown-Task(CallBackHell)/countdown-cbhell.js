8f440b4066721929c37905a9552e4c2f16df6ac5
Gobinath M <95267408+Gobinath26@users.noreply.github.com>
2022-02-01 19:14:12 +0530

    Added files via upload

---
+++ Countdown-Task(CallBackHell)/countdown-cbhell.js
@@ -0,0 +1,49 @@
+var i = 11;
+setTimeout(function () {
+  document.getElementById("hell").innerHTML = "Countdown Starts Now...";
+
+  setTimeout(function () {
+    i--;
+    document.getElementById("hell").innerHTML = i;
+    setTimeout(function () {
+      i--;
+      document.getElementById("hell").innerHTML = i;
+      setTimeout(function () {
+        i--;
+        document.getElementById("hell").innerHTML = i;
+        setTimeout(function () {
+          i--;
+          document.getElementById("hell").innerHTML = i;
+          setTimeout(function () {
+            i--;
+            document.getElementById("hell").innerHTML = i;
+            setTimeout(function () {
+              i--;
+              document.getElementById("hell").innerHTML = i;
+              setTimeout(function () {
+                i--;
+                document.getElementById("hell").innerHTML = i;
+                setTimeout(function () {
+                  i--;
+                  document.getElementById("hell").innerHTML = i;
+                  setTimeout(function () {
+                    i--;
+                    document.getElementById("hell").innerHTML = i;
+                    setTimeout(function () {
+                      i--;
+                      document.getElementById("hell").innerHTML = i;
+                      setTimeout(function () {
+                        document.getElementById("hell").innerHTML =
+                          "Happy Birthday🎂🎈🎆😍";
+                      }, 1000);
+                    }, 1000);
+                  }, 1000);
+                }, 1000);
+              }, 1000);
+            }, 1000);
+          }, 1000);
+        }, 1000);
+      }, 1000);
+    }, 1000);
+  }, 1000);
+}, 1300);
